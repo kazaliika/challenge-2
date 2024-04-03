c9d662d453a0a1f42843974353c5f3fdbe7276e9
kazaliika <mavero811@gmail.com>
2024-04-03 21:22:42 +0700

    baru

--- question1/task3.dart
+++ question1/task3.dart
@@ -2,20 +2,20 @@ void main() {
   List<int> db1_Lumba_Skor = [97, 112, 101];
   List<int> db1_Koala_Skor = [109, 95, 123];
 
-  double lumbaTotal =
+  double lumbaRata =
       db1_Lumba_Skor.reduce((a, b) => a + b) / db1_Lumba_Skor.length;
-  double koalaTotal =
+  double koalaRata =
       db1_Koala_Skor.reduce((a, b) => a + b) / db1_Lumba_Skor.length;
 
   // Memeriksa apakah kedua tim memenuhi syarat skor minimum 100
-  if (lumbaTotal >= 100 && koalaTotal >= 100) {
+  if (lumbaRata >= 100 && koalaRata >= 100) {
     // Memeriksa tim mana yang memiliki skor tertinggi
-    if (lumbaTotal > koalaTotal) {
-      print("Lumba menang dengan total skor: $lumbaTotal");
-    } else if (koalaTotal > lumbaTotal) {
-      print("Koala menang dengan total skor: $koalaTotal");
+    if (lumbaRata > koalaRata) {
+      print("Lumba menang dengan total skor: $lumbaRata");
+    } else if (koalaRata > lumbaRata) {
+      print("Koala menang dengan total skor: $koalaRata");
     } else {
-      print("Seri dengan total skor: $lumbaTotal");
+      print("Seri dengan total skor: $lumbaRata");
     }
   } else {
     print("Tidak ada tim yang memenuhi syarat skor minimum 100.");
