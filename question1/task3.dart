b98d21fe4e05d7956becda7473cf59fcba6cbb2f
kazaliika <mavero811@gmail.com>
2024-04-04 21:08:28 +0700

    fix percabangan jd lebih simple tanpa nested if

--- question1/task3.dart
+++ question1/task3.dart
@@ -7,16 +7,12 @@ void main() {
   double koalaRata =
       db1_Koala_Skor.reduce((a, b) => a + b) / db1_Lumba_Skor.length;
 
-  // Memeriksa kedua tim penuhi syarat skor minimum 100
   if (lumbaRata >= 100 && koalaRata >= 100) {
-    // cek tim mana yang punya skor tertinggi
-    if (lumbaRata > koalaRata) {
-      print("Lumba menang dengan total skor: $lumbaRata");
-    } else if (koalaRata > lumbaRata) {
-      print("Koala menang dengan total skor: $koalaRata");
-    } else {
-      print("Seri dengan total skor: $lumbaRata");
-    }
+    print(lumbaRata > koalaRata
+        ? "Lumba menang dengan total skor: $lumbaRata"
+        : lumbaRata < koalaRata
+            ? "Koala menang dengan total skor: $koalaRata"
+            : "Seri dengan total skor: $lumbaRata");
   } else {
     print("Tidak ada tim yang memenuhi syarat skor minimum 100.");
   }
